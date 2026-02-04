42598d749be508901352e6a9ab70ed8fbaf97bf6
Karan pandey <100835737+karan-pandey15@users.noreply.github.com>
2026-02-04 14:13:34 +0530

    Remove phone number display from footer
    Comment out phone number section in footer.

--- src/app/components/footer/page.js
+++ src/app/components/footer/page.js
@@ -222,12 +222,15 @@ const Footer = () => {
                 </span>
                 B-25, sector 2 noida - 201301
                </p>
-              // <p className="text-white">
-              //   <span style={{ color: "#3E9D7C", fontSize: "26px" }}>
-              //     <AiFillPhone />
-              //   </span>{" "}
-              //   +91 8887796224
-              // </p>
+            {/* 
+<p className="text-white">
+  <span style={{ color: "#3E9D7C", fontSize: "26px" }}>
+    <AiFillPhone />
+  </span>{" "}
+  +91 8887796224
+</p>
+*/}
+
               <p className="text-white">
                 <span style={{ color: "#3E9D7C", fontSize: "26px" }}>
                   <TbDeviceLandlinePhone />
